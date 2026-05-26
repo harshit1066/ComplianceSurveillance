8fa8f3e6f3391ea406643c586b75eacfafb260c1
Harshit Anand <harshitanand@AS-MAC-0449.local>
2026-05-27 01:15:06 +0530

    Initial commit

---
+++ src/main/java/harshit/anand/ComplianceSurveillance/ComplianceSurveillanceApplication.java
@@ -0,0 +1,13 @@
+package harshit.anand.ComplianceSurveillance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplianceSurveillanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplianceSurveillanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/harshit/anand/ComplianceSurveillance/ComplianceSurveillanceApplicationTests.java
@@ -0,0 +1,13 @@
+package harshit.anand.ComplianceSurveillance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplianceSurveillanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
